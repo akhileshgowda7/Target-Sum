e0f6083698f7527c8fdae6cad34c139b337e1a60
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2023-03-24 16:35:08 -0400

    refs 6-chemistry-game: Period Table JSON added

--- src/components/ChemistryGame/ChemistryGame.js
+++ src/components/ChemistryGame/ChemistryGame.js
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const ChemistryGame = () => {
+  useEffect(() => {
+    fetch("https://neelpatel05.pythonanywhere.com")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <View>
       <Text style={styles.title}>Hello</Text>
